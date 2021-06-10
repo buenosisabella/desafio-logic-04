5d7aa6eb92e627b2d0bc1f394c5858bf1811282f
19092004 <coreamilisabella@gmail.com>
2021-06-09 21:01:25 -0300

    desafio pronto

---
+++ script.js
@@ -0,0 +1,28 @@
+function appConsole(){
+    console.log("Bom Dia, Boa Tarde, Boa Noite meus queridos")
+}
+
+appConsole();
+
+function nomeApp (name){
+    console.log(name);
+}
+
+nomeApp("Isabella Bueno");
+
+function itensCompletos( nome, idade, estilo){
+    console.log(`Meu nome é ${nome}, tenho ${idade}, e meu estilo musical é ${estilo}`)
+}
+
+itensCompletos( "Isabella", 16, "gospel")
+
+
+function filmesFavoritos(filme, musica){
+    console.log(`Meu filme favorito e  ${filme}, ja minha musica favorita e ${musica}`)
+}
+
+ filmesFavoritos("As Branquelas", "I smile")
+
+ function triple(x) {
+     return x*16
+ }
